50793b49973f52412f75ad88664e0aae00bfa232
Vivienne Cruz <cruzviv27@hotmail.com>
2022-05-27 22:32:34 -0400

    basis for header

--- src/components/navigation/header/header.jsx
+++ src/components/navigation/header/header.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import style from "./header.module.scss";
 
 class Header extends Component {
     render() {
         return (
-            <h1>
-                header
-            </h1>
+            <div className={style.container}>
+                <Link to="/">
+                    <span>Home</span>
+                </Link>
+
+                <Link to="/about">
+                    <span>About</span>
+                </Link>
+            </div>
         )
     }
 }
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // import 'antd/dist/antd.css';
 
+import Layout from "../../components/layout/layout"
+
 class Landing extends Component {
     render() {
         return(
-            <div>
+            <Layout>
                 <h1>free wrld</h1>
-            </div>
+            </Layout>
         )
     }
 }
